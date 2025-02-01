ca0ba60e6b365b44d47683aa4527730dbd154e8d
LIT <littoma@hotmail.com>
2025-02-01 17:44:11 +0800

    Makes window draggable and fixes button alignment
    Attaches event to make the entire window draggable except input controls
    Fixes alignment of button properties in XAML

--- ModbusCalc/ModbusCalc/Views/MainWindow.axaml.cs
+++ ModbusCalc/ModbusCalc/Views/MainWindow.axaml.cs
@@ -7,5 +7,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        // Attach event to make the entire window draggable except input controls
+        this.PointerPressed += (sender, e) =>
+        {
+            var pointerOver = e.Source as Control;
+            if (pointerOver is Panel)
+            {
+                BeginMoveDrag(e);
+            }
+        };
     }
 }
